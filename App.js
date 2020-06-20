757df7026c7337f0ecf0367bc15bb46614a04f42
waspryo <sugimoto@liddell.tokyo>
2020-06-20 16:26:48 +0900

    [WIP]useStatusとmapで表示

--- App.js
+++ App.js
@@ -1,49 +1,40 @@
-import React, { useState } from 'react';
+import React,
+{ useState } from 'react';
 import {
   StyleSheet,
   Text,
   View,
+  TextInput,
+  Button,
   SafeAreaView
 } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
+  }
+
   return (
     <SafeAreaView>
-      <View style={{
-        padding: 50,
-        flexDirection: 'row',
-        width: '80%',
-        height: 300,
-        justifyContent: "space-around",
-        alignItems: 'stretch'
-      }}>
-        <View
-          style={{
-            backgroundColor: 'red',
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-          <Text>1</Text>
+      <View style={styles.screen}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Course Goal"
+            style={styles.input}
+            onChangeText={goalInputHandler}
+            value={enteredGoal}
+          />
+          <Button title="Add" onPress={addGoalHandler} />
         </View>
-        <View
-          style={{
-            backgroundColor: 'blue',
-            flexDirection: 'row',
-            flex: 1 ,
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-          <Text>2</Text>
-        </View>
-        <View
-          style={{
-            backgroundColor: 'green',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-          <Text>3</Text>
+        <View>
+          {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
         </View>
       </View>
     </SafeAreaView>
@@ -51,5 +42,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 10
+  }
 });
