d2cbb1f1553f533c5bb16539f52ccadcf6d44787
waspryo <sugimoto@liddell.tokyo>
2020-06-20 15:55:35 +0900

    [FlexBox]

--- App.js
+++ App.js
@@ -3,28 +3,47 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput,
-  Button,
   SafeAreaView
 } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <View style={{ padding: 40 }}>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-          <TextInput
-            placeholder="Course Goal"
-            style={{
-              width: '80%',
-              borderBottomColor: 'black',
-              borderBottomWidth: 1,
-              padding: 10
-            }}
-          />
-          <Button title="Add" />
+      <View style={{
+        padding: 50,
+        flexDirection: 'row',
+        width: '80%',
+        height: 300,
+        justifyContent: "space-around",
+        alignItems: 'stretch'
+      }}>
+        <View
+          style={{
+            backgroundColor: 'red',
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+          <Text>1</Text>
         </View>
-        <View>
+        <View
+          style={{
+            backgroundColor: 'blue',
+            flexDirection: 'row',
+            flex: 1 ,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+          <Text>2</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'green',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+          <Text>3</Text>
         </View>
       </View>
     </SafeAreaView>
